1607b60901d6993a95b1468a1892b93093713e07
LX <lixu@wsbroker.me>
2024-10-21 18:12:05 +0800

    优化

--- shareREC/build.gradle.kts
+++ shareREC/build.gradle.kts
@@ -26,6 +26,12 @@ android {
         sourceCompatibility = JavaVersion.VERSION_1_8
         targetCompatibility = JavaVersion.VERSION_1_8
     }
+
+    sourceSets {
+        getByName("main") {
+            jniLibs.srcDirs("src/main/jniLibs")
+        }
+    }
 }
 
 publishing {
